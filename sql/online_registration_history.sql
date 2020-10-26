170daf0701fa35eadbacb8ba6f4fef74a389cfc1
BenCroft <benjaminhcroft@gmail.com>
2020-10-26 17:03:54 -0600

    added two sql files

---
+++ sql/online_registration_history.sql
@@ -0,0 +1,6 @@
+SELECT * FROM CustomFInal.ReportViewFactRegistrationBasic 
+
+WHERE TermKey >= 280 and TermKey <= 310
+AND OfficiallyRegisteredIndicator = 'Officially Registered'
+AND InstructionModeDescription = 'Online'
+
