a99d5799f8e63873261822c4dbd676f8974444ca
Benjamin Croft <benjaminhcroft@gmail.com>
2020-10-28 13:16:09 -0600

    Delete obsolete query

--- sql/online_registration_history.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM CustomFInal.ReportViewFactRegistrationBasic 
-
-WHERE TermKey >= 280 and TermKey <= 310
-AND OfficiallyRegisteredIndicator = 'Officially Registered'
-AND InstructionModeDescription = 'Online'
-
