f307bda578b4475a4ed0a48134a91da1694ec984
BenCroft <benjaminhcroft@gmail.com>
2020-11-16 10:01:53 -0700

    Cleaned A4L query

--- sql/a4l-batchuids.sql
+++ sql/a4l-batchuids.sql
@@ -1,4 +1,7 @@
-SELECT * FROM Final.DimCourse
+SELECT CourseKey, Description, SubjectCode, CourseNumber, Section, 
+InstructionMethod, SectionStatus, CourseType, BatchUID 
+
+FROM Final.DimCourse
 
 WHERE BatchUID LIKE '%1203%' and BatchUID LIKE '%BIOL%' AND BatchUID LIKE '%228%'
 AND InstructionMethod = 'Online' 
